7503449cfd59db51d7cdafda0ead4a004a4684e6
Robert Rivas <robertrivas@gmail.com>
2013-03-28 21:40:40 -0700

    Traverse structure till no directories exist

--- unrar.rb
+++ unrar.rb
@@ -1,2 +1,20 @@
-ARGV.each {|x| p x}
-TOP_GEAR_DIR = "Top.Gear.S01-S18.HDTV.XviD-Mixed"
+require 'pathname'
+
+TOP_GEAR_DIR = "/home/rrivas/Downloads/complete/torrents/Top.Gear.S01-S18.HDTV.XviD-Mixed"
+
+def directories_present?(dir)
+  Dir.glob("#{dir}/*").entries.any? { |x| File.directory?(x) }
+end
+
+def read_dir dir
+  if directories_present?(dir)
+    p "checking #{dir}"
+    Dir.glob("#{dir}/*").each_with_object({}) { |f, h| read_dir(f) if File.directory?(f)}
+  else
+    p "im inside #{dir}"
+    #p dir
+    #p Dir.entries(Pathname.new(dir).parent.to_s).delete_if{ |x| x =~ /\.sfv|\.nfo/ || x == '.' || x == '..' }.sort.first
+  end
+end
+
+read_dir TOP_GEAR_DIR
