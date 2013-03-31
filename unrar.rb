d553fe9834601a1828e85215af3d37d26b04049a
Rob Rivas <rrivas@goldstar.com>
2013-03-30 22:07:20 -0700

    Set up commands by file per directory

--- unrar.rb
+++ unrar.rb
@@ -9,12 +9,20 @@ end
 def read_dir dir
   if directories_present?(dir)
     p "checking #{dir}"
-    Dir.glob("#{dir}/*").each_with_object({}) { |f, h| read_dir(f) if File.directory?(f)}
+    Dir.glob("#{dir}/*").each_with_object({}) { |f, h| read_dir(f) }
   else
-    p "im inside #{dir}"
-    #p dir
-    #p Dir.entries(Pathname.new(dir).parent.to_s).delete_if{ |x| x =~ /\.sfv|\.nfo/ || x == '.' || x == '..' }.sort.first
+    files = Dir.entries(dir).delete_if{ |x| File.directory?(x) }.sort
+    p files
+    
+    files.each do |file|
+      p "Use #{file} to unrar #{dir}"
+
+      if $stdin.gets.chomp == 'y'
+        p "lets do it"
+        break
+      end
+    end
   end
 end
 
-read_dir TOP_GEAR_DIR
+read_dir ARGV[0]
