739e3be666bb600b8fe4515b99313bc954aa00da
Robert Rivas <robertrivas@gmail.com>
2013-10-17 13:29:45 -0700

    Refactor some to make it readable

--- unrar.rb
+++ unrar.rb
@@ -1,29 +1,35 @@
-TOP_GEAR_DIR = "/home/rrivas/Downloads/complete/torrents/Top.Gear.S01-S18.HDTV.XviD-Mixed"
-
 def directories_present?(dir)
   Dir.glob("#{dir}/*").entries.any? { |x| File.directory?(x) }
 end
 
-def read_dir dir
+def sort_files(dir)
+  Dir.entries(dir).delete_if{ |x| File.directory?(x) || x =~ /\.nfo|\.sfv/  }.sort
+end
+
+def unrar_file(dir, file)
+  p "Unraring #{file}"
+
+  `unrar x #{dir}/#{file}`
+
+  unless $?.success?
+    File.open('output', 'a+') { |f| f.write "#{file}\n" }
+  else
+    `rm -rf #{dir}`
+  end
+end
+
+def read_dir(dir)
   if directories_present?(dir)
     Dir.glob("#{dir}/*").each_with_object({}) { |f, h| read_dir(f) }
   else
-    files = Dir.entries(dir).delete_if{ |x| File.directory?(x) || x =~ /\.nfo|\.sfv/  }.sort
-    
+    files = sort_files(dir)
+
     files.each do |file|
-      p "Unraring #{file}"
-
-      `unrar x #{dir}/#{file}`
-
-      unless $?.success?
-        File.open('output', 'a+') { |f| f.write "#{file}\n" }
-      else
-        `rm -rf #{dir}`
-      end
-
+      unrar_file(dir, file)
       break
     end
   end
 end
 
-read_dir ARGV[0]
+directory = ARG[0]
+read_dir directory
